2c9e53bd4ba5e0b5a4f3f50cd69d3035a9cc6ab1
Josef Baltz <josef@baltz.net>
2018-05-07 15:01:12 -0500

    Added More Detection Words and Added "Spooky The Ghost!" to Whitelist
    - Added Russian Based Characters to the Whitelist
    - Added "Spooky The Ghost!" to the whitelist for finding this bug (Thanks!)

--- src/main/java/me/orangeflare/azazelbot/commandPackages/nou.java
+++ src/main/java/me/orangeflare/azazelbot/commandPackages/nou.java
@@ -9,16 +9,16 @@ public class nou {
     @EventSubscriber
     public void onMessageReceived(MessageReceivedEvent event){
         String formattedMsg = event.getMessage().getContent().replace(" ", "").replace(",", "")
-                .replace(".", "").replace("-", "");
+                .replace(".", "").replace("-", "").toLowerCase();
         if (formattedMsg.contains("gay") || formattedMsg.contains("nou") || formattedMsg.contains("n0u") || formattedMsg.contains("g4y") ||
                 formattedMsg.contains("noyou") || formattedMsg.contains("noyu") || formattedMsg.contains("noyoo") || formattedMsg.contains("noyo") ||
                 formattedMsg.contains("ñóù") || formattedMsg.contains("ñou") || formattedMsg.contains("ñóu") || formattedMsg.contains("nóu") ||
                 formattedMsg.contains("nóù") || formattedMsg.contains("ñoù") || formattedMsg.contains("noù") || formattedMsg.contains("n0y0u") ||
-                formattedMsg.contains("noy0u") || formattedMsg.contains("n0you") || formattedMsg.contains("n0u")) {
-
+                formattedMsg.contains("noy0u") || formattedMsg.contains("n0you") || formattedMsg.contains("n0u") || formattedMsg.contains("nоu") ||
+                formattedMsg.contains("gaу")) {
             if (formattedMsg.contains("enough") || formattedMsg.contains("announce") || formattedMsg.contains("pronounce") || formattedMsg.contains("noyour")) {
                 //do nothing
-            } else if (event.getAuthor().getLongID() == 176108182056206336L || event.getAuthor().getLongID() == 253733469778083840L) {
+            } else if (event.getAuthor().getLongID() == 176108182056206336L || event.getAuthor().getLongID() == 253733469778083840L || event.getAuthor().getLongID() == 241685759348703232L) {
                 System.out.println("[INFO] Disabling Tactical No U Launch");
                 System.out.println("    Reason: " + event.getAuthor().getDisplayName(event.getGuild()) + " Sent Message");
             } else if (event.getMessage().getAuthor().isBot()){
